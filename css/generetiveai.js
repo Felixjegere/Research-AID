e8a20ba6aa12c43553639943d51f08f8e4b77298
Felixjegere <79108945+Felixjegere@users.noreply.github.com>
2024-07-14 02:43:47 -0700

    Update generetiveai.js

--- css/generetiveai.js
+++ css/generetiveai.js
@@ -1,14 +1,14 @@
 /*
 import { GoogleGenerativeAI } from "@google/generative-ai";
 // Fetch API KEY
-const API_KEY = "AIzaSyBg5n67wKsRug8mmewJ4PMpy1nPAikJlD8";
+const API_KEY = "";
 // Access your API KEY 
 const genAI = new GoogleGenerativeAI(API_KEY);
 */
 alert()/*
 import { GoogleGenerativeAI } from "@google/generative-ai";
 // Fetch API KEY
-const API_KEY = "AIzaSyBg5n67wKsRug8mmewJ4PMpy1nPAikJlD8";
+const API_KEY = "";
 // Access your API KEY 
 const genAI = new GoogleGenerativeAI(API_KEY);
 */
